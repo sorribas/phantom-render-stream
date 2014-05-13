134f8eec5444f7e4619fe713f13b81f4fe17437b
Eduardo Sorribas <eduardo@sorribas.org>
2014-05-13 16:04:11 +0200

    Update example.

--- example.js
+++ example.js
@@ -4,9 +4,4 @@ var fs = require('fs');
 var render = phantom();
 var outputStream = fs.createWriteStream('sorribas.png');
 
-// Close the phantom process when we are done streaming
-outputStream.on('finish', function () {
-  render.destroy();
-});
-
 render('http://sorribas.org').pipe(outputStream);
