69987f9f91c499d9f6c2a7199daa32dca60fa08d
Mark Stosberg <mark@rideamigos.com>
2014-05-25 12:39:51 -0400

    Tests should also use 'phantomjs' package for consistency.
    The code has been updated to use the 'phantomjs' package to install and
    locate the 'phantomjs' binary, but the tests has not.
    
    If 'phantomjs' had install 'phantomjs' locally and 'phantomjs' was not
    in $PATH, then the tests would fail, even though 'phantomjs' was
    installed would be located by the code.
    
    Now we test for phantomjs the same way the code would.

--- test/helpers/test.js
+++ test/helpers/test.js
@@ -1,15 +1,16 @@
 var tape = require('tape');
 var http = require('http');
 var proc = require('child_process');
+var phantomjsPath = require('phantomjs').path;
 
 var server;
 module.exports = function(msg, fn) {
 	tape(msg, function(t) {
 		if (server) return fn('http://localhost:'+server.address().port, t);
 
-		proc.exec('phantomjs --version', function(err) {
+		proc.exec(phantomjsPath + ' --version', function(err) {
 			if (err) {
-				t.fail('You need to install phantomjs and have it in your $PATH');
+				t.fail('phantomjs module is not properly installed. Try re-installing it. Error was: '+err);
 				process.exit(1);
 			}
 
